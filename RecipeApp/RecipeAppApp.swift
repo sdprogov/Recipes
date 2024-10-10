89f3c599255bce2ac04b38d505b1480a8dfd8fcc
Stefan Progovac <developer@discoverrestyle.com>
2024-10-10 12:32:33 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Stefan Progovac on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by Stefan Progovac on 10/10/24.
+//
+
+import Testing
+@testable import RecipeApp
+
+struct RecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
